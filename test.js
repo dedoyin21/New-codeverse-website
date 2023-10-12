1e2ab98892482b0490299e94106ed3294561494d
deedee213 <133877095+dedoyin21@users.noreply.github.com>
2023-10-12 12:02:46 +0100

    initial commit

--- test.js
+++ test.js
@@ -1,18 +1,20 @@
 // Select the SVG icon and the dropdown menu
 const menuIcon = document.getElementById("menuicon");
-const dropdownMenu = document.querySelector(".dropdown-menu");
+const menuMobile = document.querySelector(".menuMobile"); // Corrected the class name
 
 // Toggle the dropdown menu when the SVG icon is clicked
 menuIcon.addEventListener("click", () => {
-    if (dropdownMenu.style.display === "block") {
-        dropdownMenu.style.display = "none"; // Hide the menu
+    if (menuMobile.style.display === "block") {
+        menuMobile.style.display = "none"; // Hide the menu
     } else {
-        dropdownMenu.style.display = "block"; // Show the menu
+        menuMobile.style.display = "block"; // Show the menu
     }
 });
+
 // Close the dropdown when clicking outside of it
 document.addEventListener("click", (event) => {
-    if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-        dropdownMenu.style.display = "none"; // Hide the menu
+    if (!menuIcon.contains(event.target) && !menuMobile.contains(event.target)) {
+        menuMobile.style.display = "none"; // Hide the menu
     }
 });
+
