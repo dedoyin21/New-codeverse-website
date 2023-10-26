939e0410544e13a99f56801a91ab80e102bf1202
deedee213 <133877095+dedoyin21@users.noreply.github.com>
2023-10-26 12:42:35 +0100

    javascript

--- test.js
+++ test.js
@@ -1,20 +1,28 @@
-// Select the SVG icon and the dropdown menu
-const menuIcon = document.getElementById("menuicon");
-const menuMobile = document.querySelector(".menuMobile"); // Corrected the class name
+document.addEventListener("DOMContentLoaded", function () {
+    const programs = document.getElementById("programs");
+    const barsIcon = document.querySelector(".fa-solid.fa-bars");
+    const closeIcon = document.querySelector(".fa.fa-close");
 
-// Toggle the dropdown menu when the SVG icon is clicked
-menuIcon.addEventListener("click", () => {
-    if (menuMobile.style.display === "block") {
-        menuMobile.style.display = "none"; // Hide the menu
-    } else {
-        menuMobile.style.display = "block"; // Show the menu
-    }
-});
+    programs.addEventListener("click", function () {
+        if (closeIcon.style.display === "inline") {
+            // If the close icon is visible, hide it when "Programs" is clicked
+            closeIcon.style.display = "none";
+            barsIcon.style.display = "inline";
+        }
+    });
 
-// Close the dropdown when clicking outside of it
-document.addEventListener("click", (event) => {
-    if (!menuIcon.contains(event.target) && !menuMobile.contains(event.target)) {
-        menuMobile.style.display = "none"; // Hide the menu
-    }
+    barsIcon.addEventListener("click", function () {
+        if (closeIcon.style.display === "none") {
+            // If the close icon is not visible, show it when the bars icon is clicked
+            closeIcon.style.display = "inline";
+            barsIcon.style.display = "none";
+        }
+    });
+
+    closeIcon.addEventListener("click", function () {
+        // When the close icon is clicked, switch back to the bars icon
+        closeIcon.style.display = "none";
+        barsIcon.style.display = "inline";
+    });
 });
 
